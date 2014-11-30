7baa9e7d221e655fdec43def44b61ef1c30849b0
Xu Meng <xumeng@Xus-MacBook-Pro.local>
2014-11-30 10:40:10 -0500

    util scripts

---
+++ scripts/README.md
@@ -0,0 +1,1 @@
+Update ARC_SEC_DIR value in settings.sh to the corrent directory entry and add this script folder to your PATH variable. Currently works for Mac OS only.
---
+++ scripts/settings.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ARC_SEC_DIR="$HOME/bss-project/arc-security"
+RPKG_SCRIPT="chromeos-apk"
+APP_DIR="$ARC_SEC_DIR/app"
+APP_SRC_DIR="$APP_DIR/src"
+APP_APK_DIR="$APP_DIR/apk"
+APP_ARC_DIR="$APP_DIR/arc"
